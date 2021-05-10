fa92f22019d1bd81edece9022db350ba5703c47c
Gajendra <gsahu125@gmail.com>
2021-05-10 11:43:16 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h2>Welcome to React-Learning</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
