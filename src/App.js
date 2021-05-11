5123ae7695bb4574dcbeaf4bf38ab3a975bf6617
Gajendra <gsahu125@gmail.com>
2021-05-11 18:35:59 +0530

    props learning finished

--- src/App.js
+++ src/App.js
@@ -5,6 +5,16 @@ import About from './components/About';
 import Contact from './components/Contact';
 import {BrowserRouter,Route,Switch} from 'react-router-dom';
 function App() {
+
+
+  let data = [
+    {name:'gajendra',age:30,dob:'07-05-1991',maritalStatus:'married',designation:'web developer'},
+    {name:'gajendra1',age:30,dob:'07-05-1991',maritalStatus:'married',designation:'web designer'},
+    {name:'gajendra2',age:30,dob:'07-05-1991',maritalStatus:'married',designation:'full stack developer'}
+  ];
+
+  const data1 = {name:'monet',hoby:'watching tiktok',age:25}
+
   return (
    <React.Fragment>
      <BrowserRouter>
@@ -17,7 +27,7 @@ function App() {
        <Contact />
      </Route>
      <Route path="/">
-       <Home />
+       <Home data={data} monetData={data1} />
      </Route>
   </Switch>
      </BrowserRouter>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,45 @@
 import React from 'react';
-
-
-export const Home = ()=>{
+import {Table} from 'react-bootstrap';
+export const Home = (props)=>{
+const arr = props.data;
+const arr1 = props.monetData;
+// console.warn(arr1)
+let i =0;
     return(
         <React.Fragment>
-            <p>This is home component </p>
+            <br></br>
+            <h2 style={{textAlign:'center'}}>Array of object recieved by props</h2>
+            <Table striped bordered hover>
+                    <thead>
+                      <tr>
+                        <th>#</th>
+                        <th>Name</th>
+                        <th>Age</th>
+                        <th>DOB</th>
+                        <th>Marital Status</th>
+                        <th>Designation</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+           {
+               arr.map((item)=>
+               { 
+                i = i+1;
+                return(
+                      <tr>
+                        <td>{i}</td>
+                        <td>{item.name}</td>
+                        <td>{item.age}</td>
+                        <td>{item.dob}</td>
+                        <td>{item.maritalStatus}</td>
+                        <td>{item.designation}</td>
+                      </tr>
+                )
+               })
+            }
+             </tbody>
+            </Table>
         </React.Fragment>
     )
-}
+} 
  
